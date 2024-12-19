c436189c129b792bac9bb41cfd3fd7a03a968474
mmt11279_mmt <harshit.bhutani@go-mmt.com>
2024-12-19 13:22:32 +0530

    inventory app commit 3

--- src/api/fetchList.js
+++ src/api/fetchList.js
@@ -1,41 +1,4 @@
-const API_URL = "https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory";
-const mock = [
-  {
-    name: "Bluetooth",
-    category: "Electronic",
-    value: "$150",
-    quantity: 5,
-    price: "$30",
-  },
-  {
-    name: "Edifier M43560",
-    category: "Electronic",
-    value: "$0",
-    quantity: 0,
-    price: "$0",
-  },
-  {
-    name: "Sony 4k ultra 55 inch TV",
-    category: "Electronic",
-    value: "$1190",
-    quantity: 17,
-    price: "$70",
-  },
-  {
-    name: "Samsumg 55 inch TV",
-    category: "Electronic",
-    value: "$600",
-    quantity: 50,
-    price: "$12",
-  },
-  {
-    name: "samsumg S34 Ultra",
-    category: "phone",
-    value: "$0",
-    quantity: 0,
-    price: "$0",
-  },
-];
+import { API_URL, mock } from "../utils/constants";
 
 async function fetchList() {
   try {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,42 @@
+export const API_URL = "https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory";
+export const mock = [
+  {
+    name: "Bluetooth",
+    category: "Electronic",
+    value: "$150",
+    quantity: 5,
+    price: "$30",
+  },
+  {
+    name: "Edifier M43560",
+    category: "Electronic",
+    value: "$0",
+    quantity: 0,
+    price: "$0",
+  },
+  {
+    name: "Sony 4k ultra 55 inch TV",
+    category: "Electronic",
+    value: "$1190",
+    quantity: 17,
+    price: "$70",
+  },
+  {
+    name: "Samsumg 55 inch TV",
+    category: "Electronic",
+    value: "$600",
+    quantity: 50,
+    price: "$12",
+  },
+  {
+    name: "samsumg S34 Ultra",
+    category: "phone",
+    value: "$0",
+    quantity: 0,
+    price: "$0",
+  },
+];
+
 export const widgetTitleMap = {
   totalProducts: "Total Products",
   totalValue: "Total Store Value",
